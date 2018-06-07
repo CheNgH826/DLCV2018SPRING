961f7b764bfceed9c0a8de9afcd0afdb06b09d2b
Ubuntu <hung@myvm.x1ovgoc2syhebpnf50p5wz3kxc.xx.internal.cloudapp.net>
2018-06-07 10:31:54 +0000

    remove comment

--- hw3/hw3.sh
+++ hw3/hw3.sh
@@ -1,2 +1,2 @@
-#wget https://www.dropbox.com/s/8zo086m8smq8zw4/model.h5?dl=1 -O model.h5
+wget https://www.dropbox.com/s/8zo086m8smq8zw4/model.h5?dl=1 -O model.h5
 python3 infer_test.py model.h5 $1 $2
