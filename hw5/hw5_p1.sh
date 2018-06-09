89b07d7d4ae7ecbd84b65bf4588db272053b2a9d
CheNgH826 <lilioo826@gmail.com>
2018-06-09 14:49:46 +0800

    remove comment

--- hw5/hw5_p1.sh
+++ hw5/hw5_p1.sh
@@ -1,2 +1,2 @@
-#wget https://github.com/CheNgH826/hello-world/releases/download/dlcv_hw5_task1/task1_model.pth
+wget https://github.com/CheNgH826/hello-world/releases/download/dlcv_hw5_task1/task1_model.pth
 python3 task1_infer.py task1_model.pth $1 $2 $3
--- hw5/hw5_p3.sh
+++ hw5/hw5_p3.sh
@@ -1,2 +1,2 @@
-#wget https://github.com/CheNgH826/hello-world/releases/download/dlcv_hw5_task3/task3_model.pth
+wget https://github.com/CheNgH826/hello-world/releases/download/dlcv_hw5_task3/task3_model.pth
 python3 task3_infer.py task3_model.pth $1 $2
